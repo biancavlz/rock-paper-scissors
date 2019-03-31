de8c365c5ade6223aa324247df6a832e46c4ba41
Bianca <ing_biank@hotmail.com>
2019-03-31 18:02:27 +0200

    Adds opponents images

--- game.js
+++ game.js
@@ -7,28 +7,28 @@ function result(leftHand, rightHand){
   const headerSelector = document.querySelector("h1")
 
   if (leftHand === 3 && rightHand === 1){
-    return headerSelector.innerHTML = "Player2 wins";
+    return headerSelector.innerHTML = "You win";
   }else if (leftHand === 3 && rightHand === 2){
     return headerSelector.innerHTML = "Player1 wins";
   }else if (leftHand === 2 && rightHand === 1){
     return headerSelector.innerHTML = "Player1 wins";
   }else if (leftHand === 2 && rightHand === 3){
-    return headerSelector.innerHTML = "Player2 wins";
+    return headerSelector.innerHTML = "You win";
   }else if (leftHand < rightHand) {
-    return headerSelector.innerHTML = "Player2 wins";
+    return headerSelector.innerHTML = "You win";
   } else {
     return headerSelector.innerHTML = "Draw!";
   }
 }
 
-const rightHandsArray = document.querySelectorAll('button')
+const rightHandsArray = document.querySelectorAll('.button-right')
 
 rightHandsArray.forEach(function(button, i) {
   button.addEventListener("click", function() {
 
     setTimeout(function() {
       const rightHandNum = i + 1
-      document.getElementsByTagName("img")[1].setAttribute("src", "./assets/right" + rightHandNum + ".png");
+      document.getElementsByTagName("img")[6].setAttribute("src", "./assets/right" + rightHandNum + ".png");
 
       const leftHandNum = getRandomNumber();
       document.getElementsByTagName("img")[0].setAttribute("src", "./assets/left" + leftHandNum + ".png"); 
