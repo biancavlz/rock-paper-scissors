04fbf20169dceaa70b9d3abaa048fdc2f1c796bd
Bianca <ing_biank@hotmail.com>
2019-03-31 16:07:02 +0200

    Adds logic

--- game.js
+++ game.js
@@ -3,18 +3,37 @@ function getRandomNumber(){
   return number;
 }
 
-const handLeft = getRandomNumber();
-const handRight = getRandomNumber();
+function result(leftHand, rightHand){
+  const headerSelector = document.querySelector("h1")
 
-document.getElementsByTagName("img")[0].setAttribute("src", "./assets/left" + handLeft + ".png"); 
-document.getElementsByTagName("img")[1].setAttribute("src", "./assets/right" + handRight + ".png");
-
-
-const headerSelector = document.querySelector("h1")
-if (handLeft > handRight) {
-  headerSelector.innerHTML = "Player1 wins";
-} else if (handLeft < handRight) {
-  headerSelector.innerHTML = "Player2 wins";
-} else {
-  headerSelector.innerHTML = "Draw!";
+  if (leftHand === 3 && rightHand === 1){
+    return headerSelector.innerHTML = "Player2 wins";
+  }else if (leftHand === 3 && rightHand === 2){
+    return headerSelector.innerHTML = "Player1 wins";
+  }else if (leftHand === 2 && rightHand === 1){
+    return headerSelector.innerHTML = "Player1 wins";
+  }else if (leftHand === 2 && rightHand === 3){
+    return headerSelector.innerHTML = "Player2 wins";
+  }else if (leftHand < rightHand) {
+    return headerSelector.innerHTML = "Player2 wins";
+  } else {
+    return headerSelector.innerHTML = "Draw!";
+  }
 }
+
+const rightHandsArray = document.querySelectorAll('button')
+
+rightHandsArray.forEach(function(button, i) {
+  button.addEventListener("click", function() {
+
+    setTimeout(function() {
+      const rightHandNum = i + 1
+      document.getElementsByTagName("img")[1].setAttribute("src", "./assets/right" + rightHandNum + ".png");
+
+      const leftHandNum = getRandomNumber();
+      document.getElementsByTagName("img")[0].setAttribute("src", "./assets/left" + leftHandNum + ".png"); 
+
+      result(leftHandNum, rightHandNum);
+    }, 2000);
+  });
+});
