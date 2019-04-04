c452ef9a2ad07681d8f525acbc3768d86b46e2a4
Bianca <ing_biank@hotmail.com>
2019-04-04 11:35:54 +0200

    refactoring

--- game.js
+++ game.js
@@ -24,42 +24,51 @@ function setsValues(valueLeft, valueRight){
   cellRight.innerHTML  = valueRight;
 }
 
-const valueLeft = 0
+// function resetScore(){
+//   const opponentsArray = document.querySelectorAll('.opponent')
+
+//   return opponentsArray.forEach(function(button, i) {
+//     button.addEventListener("click", function() {
+//       let name    = document.getElementsByTagName("button")[i].textContent
+//       console.log(name)
+//     });
+//   });
+// }
+
+const valueLeft  = 0
 const valueRight = 0
 
 function result(leftHand, rightHand){
   const headerSelector = document.querySelector("h1")
-  const name = document.querySelector(".player-one").textContent
-
-  
+  const opponentName = document.querySelector(".player-one").textContent
 
   let insert = headerSelector.innerHTML
 
   if (leftHand === 3 && rightHand === 1){
-    score(name, valueLeft, insert = "1");
+    score(opponentName, valueLeft, insert = "1");
     return headerSelector.innerHTML = "You win";
 
   }else if (leftHand === 3 && rightHand === 2){
-    score(name, insert = "1", valueRight);
-    return headerSelector.innerHTML = name + " wins";
+    score(opponentName, insert = "1", valueRight);
+    return headerSelector.innerHTML = opponentName + " wins";
 
   }else if (leftHand === 2 && rightHand === 1){
-    score(name, insert = "1", valueRight);
-    return headerSelector.innerHTML = name + " wins";
+    score(opponentName, insert = "1", valueRight);
+    return headerSelector.innerHTML = opponentName + " wins";
 
   }else if (leftHand === 1 && rightHand === 3){
-    score(name, insert = "1", valueRight);
-    return headerSelector.innerHTML = name + " wins";
+    score(opponentName, insert = "1", valueRight);
+    return headerSelector.innerHTML = opponentName + " wins";
 
   }else if (leftHand === 2 && rightHand === 3){
-    score(name, valueLeft, insert = "1");
+    score(opponentName, valueLeft, insert = "1");
     return headerSelector.innerHTML = "You win";
 
   }else if (leftHand < rightHand) {
-    score(name, valueLeft,  insert = "1");
+    score(opponentName, valueLeft,  insert = "1");
     return headerSelector.innerHTML = "You win";
   } else {
-    score(name, insert = "0", insert = "0");
+    score(opponentName, insert = "0", insert = "0");
     return headerSelector.innerHTML = "Draw!";
     
   }
@@ -75,19 +84,28 @@ opponentsArray.forEach(function(button, i) {
 });
 
 
+
+function getOpponentImages(i) {
+  const rightHandNum = i + 1
+  document.getElementsByTagName("img")[2].setAttribute("src", "./assets/right" + rightHandNum + ".png");
+
+  const leftHandNum = getRandomNumber();
+  document.getElementsByTagName("img")[1].setAttribute("src", "./assets/left" + leftHandNum + ".png"); 
+
+  result(leftHandNum, rightHandNum);
+}
+
+//main function
 const rightHandsArray = document.querySelectorAll('.button-right')
 
-rightHandsArray.forEach(function(button, i) {
-  button.addEventListener("click", function() {
+function getHands(array) {
+  array.forEach(function(button, i) {
+    button.addEventListener("click", function() {
 
-    setTimeout(function() {
-      const rightHandNum = i + 1
-      document.getElementsByTagName("img")[2].setAttribute("src", "./assets/right" + rightHandNum + ".png");
-
-      const leftHandNum = getRandomNumber();
-      document.getElementsByTagName("img")[1].setAttribute("src", "./assets/left" + leftHandNum + ".png"); 
-
-      result(leftHandNum, rightHandNum);
-    }, 1500);
+      setTimeout(getOpponentImages(i), 1500);
+    });
   });
-});
+}
+
+getHands(rightHandsArray)
+
